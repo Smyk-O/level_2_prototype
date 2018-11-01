fdb9cf9775cd56a11184325229e55d604eeaf941
Oleg Smyk <Smyk006@yandex.ru>
2018-11-01 20:48:16 +0300

    upload production

---
+++ docs/precache-manifest.287325ba2459ae7392db6d758e717c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79d68011797f7124dd17f3a480a7a01f",
+    "url": "/static/media/background-img.79d68011.png"
+  },
+  {
+    "revision": "7ce02eda8d82ba68899ab63070b5f86a",
+    "url": "/static/media/Cat.7ce02eda.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96409b39031fbc780d78",
+    "url": "/static/js/main.96409b39.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "96409b39031fbc780d78",
+    "url": "/static/css/main.af55f4e3.chunk.css"
+  },
+  {
+    "revision": "1b1ae5b88b160e4f3b3dab8458abb2e8",
+    "url": "/index.html"
+  }
+];
